a33a8ab2c00a45e8ba570357eaa2d016b7e3a942
run-house <>
2023-08-24 19:14:19 +0000

    Update from https://github.com/run-house/runhouse/commit/4e5fc6b076b9b6a113cee0e43a53d583f6438b66

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,7 +1,7 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
     VERSION: '0.0.11',
-    LANGUAGE: 'None',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'json',
     FILE_SUFFIX: '.fjson',
